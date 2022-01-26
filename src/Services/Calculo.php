991347cf0c68e4123c3fc43e49fa0c51ac5ba82c
Angelo <cupertinoangelo13@gmail.com>
2022-01-26 15:29:47 -0300

    :guitar: feat: Criar comandos de calculo

---
+++ src/Services/Calculo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Angelo\Criptobot\Services;
+
+use Angelo\Criptobot\Services\ApiService;
+use Angelo\Criptobot\Services\SendEmail;
+use GuzzleHttp\Client;
+
+Class Calculo{
+
+    public function constantVerify(): void
+    {
+        $api = new ApiService(new Client);
+        $email = new SendEmail();
+        while (true) {
+            $lastValue = round($api->get('BTC')['last'], 2);
+            if ($lastValue >= 250000){
+                $email->send("CORRE E VENDE! BIT COIN BATEU {$lastValue}");
+                break;
+            }
+            if ($lastValue <= 150000){
+                $email->send("ATENCAO! BIT COIN BATEU {$lastValue}");
+                break;
+            }
+            sleep(3600);
+        }
+
+    }
+}
