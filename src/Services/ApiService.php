5584b0ab32eda693c3bc870ec14e88cca99be156
Angelo <cupertinoangelo13@gmail.com>
2022-01-26 15:35:39 -0300

    :guitar: feat: Criar service consumir API

---
+++ src/Services/ApiService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Angelo\Criptobot\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class ApiService {
+
+    public function __construct(
+        Client $client
+    )
+    {
+        $this->client = $client;
+    }
+
+    public function get(string $moeda): array
+    {
+        $uri = "https://www.mercadobitcoin.net/api/{$moeda}/ticker/";
+
+        try {
+            $response = $this->client->get($uri);
+            $array = json_decode($response->getBody(), true);
+            return $array['ticker'];
+
+        } catch (RequestException $exception) {
+            return [
+                'error' => $exception->getMessage()
+            ];
+        }
+    }
+
+}
