bffc3ed11793e6e0e5d5cb782a86c20413851393
Angelo <cupertinoangelo13@gmail.com>
2022-01-27 11:45:39 -0300

    :guitar: feat: Adicionar Entity Coin

---
+++ src/Entity/Coin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Angelo\Criptobot\Entity;
+
+/**
+ * @Entity
+ */
+class Coin
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @Column(type="string")
+    */
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+}
