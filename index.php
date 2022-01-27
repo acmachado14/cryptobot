2c0603991b5eb5a8d91fcbb6054531389bf5aa72
Angelo <cupertinoangelo13@gmail.com>
2022-01-27 13:34:43 -0300

    :recycle: refactor: Ajustar index com calculete

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Angelo\Criptobot\Services\Calculo;
+use Angelo\Criptobot\Services\Calculate;
 use Dotenv\Dotenv;
 
 require 'vendor/autoload.php';
@@ -8,5 +8,5 @@ require 'vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$calculo = new Calculo();
+$calculo = new Calculate();
 $calculo->constantVerify();
