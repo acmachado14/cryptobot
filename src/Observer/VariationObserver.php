f57fef301aa40eb4bbf22229ab2f5f3bebce55a1
Angelo <cupertinoangelo13@gmail.com>
2022-01-28 18:50:18 -0300

    :guitar: feat: Criar Observer para variation

---
+++ src/Observer/VariationObserver.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Angelo\Criptobot\Observer;
+
+use Angelo\Criptobot\Services\SendEmail;
+use SplObserver;
+use SplSubject;
+
+class VariationObserver implements SplObserver
+{
+    private $mensagem;
+    private $sendEmail;
+
+    public function __construct(string $mensagem, SendEmail $sendEmail)
+    {
+        $this->mensagem = $mensagem;
+        $this->sendEmail = $sendEmail;
+    }
+
+    public function update(SplSubject $subject): void
+    {
+        printf(
+            $this->mensagem
+        );
+        //$this->sendEmail->send($this->mensagem);
+    }
+}
