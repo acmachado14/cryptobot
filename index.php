e1aae09023ed0255a990917052217f51d90d928e
Angelo <cupertinoangelo13@gmail.com>
2022-02-21 10:51:38 -0300

    :recycle: refactor: Ajustar entrada de moedas

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
 
+use Angelo\Criptobot\Controller\CoinController;
+use Angelo\Criptobot\Entity\Coin;
+use Angelo\Criptobot\Entity\Variation;
+use Angelo\Criptobot\Helper\EntityManagerFactory;
 use Angelo\Criptobot\Service\Calculate;
 use Dotenv\Dotenv;
 
-require 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$calculo = new Calculate();
+$entityManagerFactory = new EntityManagerFactory();
+
+$coinController = new CoinController($entityManagerFactory->getEntityManager());
+
+if (sizeof($coinController->coins()) < 1){
+    $coin = new Coin('BTC');
+    $coinController->create($coin);
+}
+
+$calculo = new Calculate($entityManagerFactory->getEntityManager());
 $calculo->constantVerify();
--- src/Service/Calculate.php
+++ src/Service/Calculate.php
@@ -2,22 +2,39 @@
 
 namespace Angelo\Criptobot\Service;
 
+use Angelo\Criptobot\Controller\CoinController;
+use Angelo\Criptobot\Controller\VariationController;
+use Angelo\Criptobot\Entity\Variation;
 use Angelo\Criptobot\Handler\VariationHandler;
 use Angelo\Criptobot\Service\ApiService;
-use Angelo\Criptobot\Service\SendEmail;
+use DateTime;
+use Doctrine\ORM\EntityManager;
 use GuzzleHttp\Client;
 
 Class Calculate
 {
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function constantVerify(): void
     {
         $api = new ApiService(new Client);
+        $coinController = new CoinController($this->entityManager);
+        $coins = $coinController->coins();
+        $variationController = new VariationController($this->entityManager);
         while (true) {
-            $lastValue = round($api->get('BTC')['last'], 2);
-            //$variationHandler = new VariationHandler();
-            echo "valor do BTC as atual: {$lastValue} \n";
+            foreach ($coins as $coin){
+                $lastValue = round($api->get($coin->getName())['last'], 2);
+                $variation = new Variation($coin,$lastValue, new DateTime());
+                $variationController->create($variation);
+                new VariationHandler($variation);
+                echo "valor do BTC as atual: {$lastValue} \n";
+            }
             sleep(1800);
         }
-
     }
 }
