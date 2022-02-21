04dfe599a5a62aa36ef705964a3fc21e7048e891
Angelo <cupertinoangelo13@gmail.com>
2022-02-21 15:22:10 -0300

    :test_tube: test: Ajustar Observer test

--- tests/Unit/VariationObserverTest.php
+++ tests/Unit/VariationObserverTest.php
@@ -2,23 +2,26 @@
 
 namespace Test\Unit;
 
+use Angelo\Criptobot\Entity\Coin;
 use Angelo\Criptobot\Entity\Variation as EntityVariation;
 use Angelo\Criptobot\Model\Variation;
 use Angelo\Criptobot\Observer\VariationObserver;
 use Angelo\Criptobot\Service\SendEmail;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class VariationObserverTest extends TestCase
 {
     public function testShouldCreateVariationObservers(): void
     {
+        $coin = new Coin('BTC');
         $mensagem1 = "Goes Up";
         $mensagem2 = "Goes Down";
 
         $variationUpObserver = new VariationObserver($mensagem1, new SendEmail);
         $variationDownObserver = new VariationObserver($mensagem2, new SendEmail);
 
-        $variation = new Variation(new EntityVariation);
+        $variation = new Variation(new EntityVariation($coin, 200000, new DateTime,));
 
         $variation->attach($variationUpObserver);
         $variation->attach($variationDownObserver);
